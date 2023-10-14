f1d0e64d2edbef1989b9c063603947cf2af8e37c
Torchtopher <torchtopher@gmail.com>
2023-10-14 15:56:08 -0400

    update

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ pygame.init()
 
 DIALOGUE = ["Hello there I am Orestes. You are a factory worker in the 1800s. Your goal is to survive. If you are lucky you might have a chance to live a miserable life. More than likley you wil die.              Click anywhere to continue."]
 DIALOGUE.append("To play, simply click the loom, after you click enough times you will get your weekly wage. If you don't have enough money to pay for living things you will die. Click anywhere to continue.")
+DIALOGUE.append("Now you may notice that clicking a black box is not very fun. You are correct. Do you work or you will die. Click anywhere to continue.")
 DIALOGUE.append("You can track your progress in the top right corner. When you character has pushed the rock to the top of the hill, you will have won the game. Click anywhere to continue.")
 # colors
 WHITE = (255, 255, 255)
@@ -76,7 +77,7 @@ class Rectangle:
             return False
 
 # rectangle for money
-money_rect = Rectangle(100, 100, 200, 200, BLACK)
+money_rect = Rectangle(100, 300, 200, 200, BLACK)
 # move rectangle to right side of screen
 money_rect.rect.x = WIDTH-money_rect.rect.width-100
 clicks_since_last_pay = 0
@@ -103,6 +104,7 @@ def _talk(text):
         screen.blit(FONT.render(line, True, BLACK), (STARTING_X, STARTING_Y))
         STARTING_Y += 30
 
+
 def say_message(text):
     global draw_text
     global current_text
@@ -112,23 +114,35 @@ def say_message(text):
 def advance_intro():
     global current_text
     global user_has_into
+    if draw_text:
+        screen.blit(speech_bubble, (200, HEIGHT-character.get_height()-speech_bubble.get_height()+350))
+        _talk(current_text)
     current_text = DIALOGUE.pop(0)
     if len(DIALOGUE) == 0:
         user_has_into = True
 
-def die_screen():
+def die_screen(msg=None):
     # make screen go black
     screen.fill(BLACK)
     # display text in red and different font
     FONT = pygame.font.SysFont("Arial", 100)
     screen.blit(FONT.render("You died", True, RED), (WIDTH/2-100, HEIGHT/2-100))
+    if msg:
+        screen.blit(FONT.render(msg, True, RED), (WIDTH/2-900, HEIGHT/2))
     pygame.display.flip()
     time.sleep(3)
     # launch game again and kill this one
     os.execl(sys.executable, sys.executable, *sys.argv)
 
+last_update = time.time()
 
 while running:
+    # every 7 seconds, subtract weekly expenses from money
+    if user_has_into and time.time() - last_update >= 7:
+        money -= 5
+        
+        last_update = time.time()
+
     pos = pygame.mouse.get_pos()
 
     # draw background
@@ -155,12 +169,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if user_has_into:
-                # click off of dialogue
-                draw_text = False
+
             if not user_has_into:
                 advance_intro()
-
+                if user_has_into:
+                    # click off of dialogue
+                    draw_text = False
             # check if money_rect was clicked
             if money_rect.click(pos):
                 # play sound effect randomly 
@@ -168,16 +182,15 @@ while running:
                     sound_effects[random.randint(0, len(sound_effects)-1)].play()
                 
                 if random.randint(1, 1000) == 1:
-                    die_screen()
+                    die_screen(msg="Unable to pay for living expenses")
                 clicks_since_last_pay += 1
                 total_clicks += 1
                 print(f"clicked {total_clicks} times ")
 
             if clicks_since_last_pay >= needed_clicks:
-                money += round(random.random(), 2)
+                money += round(random.random()*2, 2)
                 clicks_since_last_pay = 0
-                needed_clicks = random.randint(10, 20)
-    # 1/1000 chance of dying each click
+                needed_clicks = random.randint(5, 10)
 
             
     # flip the display
